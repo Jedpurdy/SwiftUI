60a35968c51ce193e323ccbef03460f9b42e4e1a
Jed PURDY <purdyj@d250-m10.iut-acy.local>
2025-01-20 09:07:24 +0100

    creation

---
+++ SwiftUIApp/SwiftUIApp/SwiftUIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIAppApp.swift
+//  SwiftUIApp
+//
+//  Created by Jed PURDY on 1/20/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
